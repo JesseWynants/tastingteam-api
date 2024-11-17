4bf4e80d76f5eb3cac4307c52ad8df7dd995c74e
Jesse Wynants <jesse@prezly.com>
2024-11-17 13:39:32 +0100

    added users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { WinesModule } from './wines/wines.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, WinesModule],
+  imports: [PrismaModule, WinesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
