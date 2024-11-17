ed55b80ebb40654d68ba80b912d84d5ef098daf5
Jesse Wynants <jesse@prezly.com>
2024-11-17 13:53:53 +0100

    removed password from the APIresponse

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,32 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+
+export class UserEntity implements User {
+    constructor(partial: Partial<UserEntity>) {
+        Object.assign(this, partial);
+      }
+    @ApiProperty()
+    id: string;
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    email: string;
+    
+    @Exclude()
+    password: string;
+
+    @ApiProperty()
+    emailVerified: Date;
+
+    @ApiProperty()
+    image: string;
+}
