2d405922320c0d8626f80857361591155f6a59be
Jesse Wynants <jesse@prezly.com>
2024-11-18 16:26:13 +0100

    added resource for scores

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { WinesModule } from './wines/wines.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ScoresModule } from './scores/scores.module';
 
 @Module({
-  imports: [PrismaModule, WinesModule, UsersModule, AuthModule],
+  imports: [PrismaModule, WinesModule, UsersModule, AuthModule, ScoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
